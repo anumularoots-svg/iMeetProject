6ed5e0045d00738397e0c1342845d613a914e0f7
anumularoots-svg <anumula.roots@gmail.com>
2025-12-29 12:38:23 +0530

    Update main.tf

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -5,7 +5,6 @@
 resource "aws_security_group" "bastion" {
   name        = "${var.name_prefix}-bastion-sg"
   description = "Security group for bastion host"
-  vpc_id      = var.vpc_id
 
   ingress {
     description = "SSH"
