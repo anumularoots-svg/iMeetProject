b62d105284927e0f5a79a9f4c2ab27a9cab16891
anumularoots-svg <anumula.roots@gmail.com>
2026-01-02 11:05:24 +0530

    Update manage.py

--- BackEnd/manage.py
+++ BackEnd/manage.py
@@ -1,25 +1,4 @@
 #!/usr/bin/env python
-import os
-import sys
-import os
-
-# ✅ Force ONNXRuntime to pick the correct GPU before Django loads anything
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["ORT_DEVICE_ID"] = "0"
-os.environ["ORT_CUDA_UNIFIED_MEMORY"] = "1"
-os.environ["ORT_CUDA_CONTEXT_ALLOW_MULTIPLE_DEVICES"] = "1"
-
-
-# FORCE SOFTWARE-ONLY PROCESSING BEFORE ANY IMPORTS
-os.environ.update({
-    'CUDA_VISIBLE_DEVICES': '',
-    'LIVEKIT_DISABLE_HARDWARE_ACCELERATION': '1',
-    'WEBRTC_DISABLE_HW_DECODER': '1',
-    'WEBRTC_DISABLE_HW_ENCODER': '1'
-})
-
-
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
