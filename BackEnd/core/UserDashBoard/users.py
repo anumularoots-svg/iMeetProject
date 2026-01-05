4184f3e96064958430ebc1e8388f2b1e2af24c72
anumularoots-svg <anumula.roots@gmail.com>
2026-01-05 19:35:34 +0530

    Update users.py

--- BackEnd/core/UserDashBoard/users.py
+++ BackEnd/core/UserDashBoard/users.py
@@ -79,6 +79,7 @@ s3_client = boto3.client(
 
 # MongoDB Configuration
 MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB", "connectlydb")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client[os.getenv("MONGO_DB", "imeetpro")]
 profile_photos_collection = db["profile_photos"]
